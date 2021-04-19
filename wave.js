ee4f8d7db6f2b500e2d25210e8e382ccd972216a
Mutanium <info@gaetan.nl>
2021-04-19 17:37:04 +0200

    puntjes op de i

--- wave.js
+++ wave.js
@@ -10,32 +10,31 @@
 
 // loop maken waarbij de uitkomst hoofdletters maakt
 
-const lower = 'hello';
-const wave = []; //vast een array klaar zetten
+
+const wave = []; //vast een array klaar zetten "scope"
 function waver (string) {
+    wave.length = 0;
     for (i = 0; i < string.length; i++) {
-        //upper = tekst aan de voorkant van 0 tot aan positie hoofdletter
-        //hoofdletter op de i'de positie
-        //originele kleine letters na de i'de positie
-        upper = string.substring(0, i).toLowerCase() + string.charAt(i).toUpperCase() + string.substring(i + 1).toLowerCase();
+        //capStrng = tekst aan de voorkant van 0 tot aan positie hoofdletter
+        //dan hoofdletter op de i'de positie
+        //dan originele kleine letters na de i'de positie
+        capStrng = string.substring(0, i).toLowerCase() + string.charAt(i).toUpperCase() + string.substring(i + 1).toLowerCase();
         //console.log (upper);
-        wave.push(upper); //aangepaste strings toevoegen aan array
+        wave.push(capStrng); //aangepaste strings toevoegen aan array
     }
     return wave;
 }
 console.log("Opdracht 1:");
 waver("gaetan");
-console.log(...wave);
-console.log("\n");
-wave.length = 0; //array leegmaken voor nieuwe input
+console.log("gaetan wordt: "+ wave);
+
 console.log("Opdracht 2:")
 waver("mSnTaaL");
-console.log(...wave);
-console.log("\n");
-wave.length = 0; //array leegmaken voor nieuwe input
+console.log("mSnTaaL wordt: "+ wave);
+
 console.log("Opdracht 3:")
 waver("hi-ya"); //ik heb niks hoeven aanpassen om het - teken overgeslagen te krijgen...
-console.log(...wave);
+console.log("hi-ya wordt: "+ wave);
 
 
 
